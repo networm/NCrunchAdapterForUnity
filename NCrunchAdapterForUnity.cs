de03d550154fb4f4a267dd66ebfb5ff0a54c40ac
networm <zzjbcn@gmail.com>
2017-10-05 00:00:16 +0800

    Add NCrunchAdapterForUnity

---
+++ NCrunchAdapterForUnity.cs
@@ -0,0 +1,22 @@
+﻿using SyntaxTree.VisualStudio.Unity.Bridge;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class NCrunchAdapterForUnity
+{
+    public const string NUnitUnityReference = @"<Reference Include=""nunit.framework"">
+      <HintPath>Library\UnityAssemblies\nunit.framework.dll</HintPath>
+    </Reference>";
+
+    public const string NUnitOfficialReference = @"<Reference Include=""nunit.framework"">
+      <HintPath>Assets\Plugins\Editor\NCrunchAdapterForUnity\NUnit.3.5.0\lib\net35\nunit.framework.dll</HintPath>
+    </Reference>";
+
+    static NCrunchAdapterForUnity()
+    {
+        ProjectFilesGenerator.ProjectFileGeneration += (string name, string content) =>
+        {
+            return content.Replace(NUnitUnityReference, NUnitOfficialReference);
+        };
+    }
+}
